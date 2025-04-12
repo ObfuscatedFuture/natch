12255a88efe9b6e75f7c52e926ea624f04c7b928
m-jhin <maxjin125@gmail.com>
2025-04-12 16:21:54 -0400

    pull?

--- src/Processing.js
+++ src/Processing.js
@@ -43,7 +43,7 @@ export default class Code {
 
     trainFun() {
         const train = ["def train(model, inputdata, outputdata, numepochs):", "\tfor epoch in range(numepochs):", "\t\tmodel.train()",
-            "\t\toptimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)", `\t\tloss = nn.${this.network.lossFunction}()`,
+            "\t\toptimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)", "\t\toptimizer.zero_grad()", `\t\tloss = nn.${this.network.lossFunction}()`,
             "\t\toutputs = model(inputdata)", "\t\terror = loss(outputs,  Y)", "\t\terror.backward()", "\t\toptimizer.step()"
         ];
 
