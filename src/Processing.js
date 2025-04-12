945bf3b69eac021aa8fdf66fb644ff42a17c5fe8
m-jhin <maxjin125@gmail.com>
2025-04-12 15:18:56 -0400

    PULL PULL PULL PULL

--- src/Processing.js
+++ src/Processing.js
@@ -11,13 +11,14 @@ class Code {
             class MLP(nn.Module)
                 
         */
-        const method = ["import torch.nn as nn", "class MLP(nn.Module):", "\tdef __init__(self):", "\t\tsuper(MLP, self).__init__(), \t\tself.model = nn.Sequential("];
+        const method = ["import torch.nn as nn", "import torch.optim as optim", "class MLP(nn.Module):", "\tdef __init__(self):", "\t\tsuper(MLP, self).__init__(), \t\tself.model = nn.Sequential()"];
         const layers = this.layersToCode();
         const code = method.concat(layers);
 
         const step = ["\tdef forwards(self, x):", "\t\treturn self.model(x)"];
         const code2 = code.concat(step);
-        return code2;
+        const code3 = code2.concat(this.trainFun());
+        return code3;
     }
 
     layersToCode() {
@@ -39,5 +40,14 @@ class Code {
         code.push(`\t\t\tnn.${this.networks.layers[i].activationFunction}())`);
 
         return code;
+    }
+
+    trainFun() {
+        const train = ["def train(model, inputdata, outputdata, numepochs):", "\tfor epoch in range(numepochs):", "\t\tmodel.train()",
+            "\t\toptimizer = optim.SGD(model.parameters(), lr=0.001, momentum=0.9)", `\t\tloss = nn.${this.network.lossFunction}`,
+            "\t\toutputs = model(inputdata)", "\t\toptimizer(outputs,  Y)", "\t\tloss.backward()", "\t\toptimizer.step()"
+        ];
+
+        return train;
     }
 }
