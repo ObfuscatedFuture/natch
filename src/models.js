bd6cde9c88d7359fd8d4b2d7b7b3fcaefd63b6e0
Varun Singh <VSpoet49@gmail.com>
2025-04-12 13:01:38 -0400

    export models

--- src/models.js
+++ src/models.js
@@ -1,10 +1,10 @@
-const LossFunction = Object.freeze({
+export const LossFunction = Object.freeze({
     CROSS_ENTROPY: Symbol("CROSS_ENTROPY"),
     MEAN_SQUARED_ERROR: Symbol("MEAN_SQUARED_ERROR"),
     L1_LOSS: Symbol("L1_LOSS"),
 })
 
-const ActivationFunction = Object.freeze({
+export const ActivationFunction = Object.freeze({
     RELU: Symbol("RELU"),
     SIGMOID: Symbol("SIGMOID"),
     TANH: Symbol("TANH"),
@@ -20,7 +20,7 @@ const ActivationFunction = Object.freeze({
  * @param {Layer[]} layers 
  * @param {LossFunction} lossFunction 
  */
-function NeuralNetwork(inputSize, outputSize, layers, lossFunction) {
+export function NeuralNetwork(inputSize, outputSize, layers, lossFunction) {
     this.inputSize = inputSize;
     this.outputSize = outputSize;
     this.layers = layers
@@ -32,7 +32,7 @@ function NeuralNetwork(inputSize, outputSize, layers, lossFunction) {
  * @param {number} numNodes 
  * @param {ActivationFunction} activationFunction 
  */
-function Layer(numNodes, activationFunction) {
+export function Layer(numNodes, activationFunction) {
     this.numNodes = numNodes;
     this.activationFunction = activationFunction;
 }
