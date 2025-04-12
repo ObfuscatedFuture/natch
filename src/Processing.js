06b16ff4ec888136b5322923ab72c07e9a39e962
kevinli3633 <kevinli3633@gmail.com>
2025-04-12 18:01:45 -0400

    Added commas to sequential

--- src/Processing.js
+++ src/Processing.js
@@ -27,16 +27,16 @@ export default class Code {
         for (let i = 0; i < this.network.layers.length - 1; i++) {
             const input = this.network.layers[i].numNodes;
             const output = this.network.layers[i + 1].numNodes;
-            const linear = `\t\t\tnn.Linear(in_features = ${input}, out_features = ${output})`;
+            const linear = `\t\t\tnn.Linear(in_features = ${input}, out_features = ${output}),`;
             
             code.push(linear);
 
-            const activation = `\t\t\tnn.${this.network.layers[i].activationFunction}()`;
+            const activation = `\t\t\tnn.${this.network.layers[i].activationFunction}(),`;
 
             code.push(activation);
         }
 
-        code.push(`\t\t\tnn.Linear(in_features = ${this.network.layers[this.network.layers.length - 1].numNodes}, out_features = ${this.network.outputSize})`);
+        code.push(`\t\t\tnn.Linear(in_features = ${this.network.layers[this.network.layers.length - 1].numNodes}, out_features = ${this.network.outputSize}),`);
         code.push(`\t\t\tnn.${this.network.layers[this.network.layers.length - 1].activationFunction}()`);
         code.push("\t\t)");
 
