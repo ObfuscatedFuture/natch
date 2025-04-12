ea3268508081ebc71937e4eba7e826cbefb9c163
kevinli3633 <kevinli3633@gmail.com>
2025-04-12 14:31:30 -0400

    Fixing processing.js

--- src/Processing.js
+++ src/Processing.js
@@ -19,17 +19,17 @@ class Code {
         for (let i = 0; i < this.network.layers.length - 1; i++) {
             const input = this.network.layers[i].numNodes;
             const output = this.network.layers[i + 1].numNodes;
-            const linear = `\t\t\tnn.Linear(in_features = ${input}, out_features = ${output})\n`;
+            const linear = `\t\t\tnn.Linear(in_features = ${input}, out_features = ${output})`;
             
             code.push(linear);
 
-            const activation = `\t\t\tnn.${this.networks.layers[i].activationFunction}()\n`;
+            const activation = `\t\t\tnn.${this.networks.layers[i].activationFunction}()`;
 
             code.push(activation);
         }
 
         code.push(`\t\t\tnn.Linear(in_features = ${this.network.layers[this.network.layers.length - 1].numNodes}, out_features = ${this.network.outputSize}\n`);
-        code.push(`\t\t\tnn.${this.networks.layers[i].activationFunction}())\n`);
+        code.push(`\t\t\tnn.${this.networks.layers[i].activationFunction}())`);
 
         return code;
     }
